892714e9db6d8ce9854deb32620b58b0250f187d
IsratJn <israt9626@gmail.com>
2021-09-08 02:33:56 +0600

    values and variables

--- Lesson 1/starter/script.js
+++ Lesson 1/starter/script.js
@@ -6,3 +6,16 @@ console.log("Jackson");
 
 let myfavSong = "The Backpack Song";
 console.log(myfavSong);
+
+let myFavSong ="The Backpack Song";
+console.log(myfavSong +'\n' + "Here i used camel convention naming my variable");
+
+
+let Fname="Azzy";
+console.log(Fname);
+
+let _name = "Ranjana";
+console.log(_name);
+
+let PI = 3.1416;
+console.log("I have declared a constant here and that is "+ PI);
