d46dd1697a4ca050934a1b5a7efbf0fc1ed86eed
IsratJn <israt9626@gmail.com>
2021-09-08 04:39:48 +0600

    assignment 3

---
+++ Lesson 1/starter/Assignment3.js
@@ -0,0 +1,11 @@
+console.log("After modification:")
+language ="Bangla";
+const isIsland = "true";
+const country = "Bangladesh";
+let population ="20 crores";
+
+console.log(country);
+console.log( population);
+
+console.log(isIsland);
+console.log(language);
