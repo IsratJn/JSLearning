96dd76bc045b149e5dedf38cda7d87948f9b153b
IsratJn <israt9626@gmail.com>
2021-09-08 03:51:13 +0600

    assignment 2

---
+++ Lesson 1/starter/Assignment 2.js
@@ -0,0 +1,11 @@
+let isIsland = "true";
+let language;
+let country = "Bangladesh";
+let population ="20 crores";
+
+console.log(typeof country);
+console.log(typeof population);
+
+console.log(typeof isIsland);
+console.log(typeof language);
+
