140656284f6bb1f3337d2fd9ea796fc719450e8a
isratJ <israt9626@gmail.com>
2021-09-08 01:32:33 +0600

    Day 2

--- Lesson 1/starter/script.js
+++ Lesson 1/starter/script.js
@@ -2,3 +2,7 @@ let js = 'amazing';
 if (js == 'amazing') alert('JavaScript is cool');
 
 console.log(50 + 9 - 42);
+console.log("Jackson"); 
+
+let myfavSong = "The Backpack Song";
+console.log(myfavSong);
