b5105d478a6d95538bf5d5d24222a310e28ef7e2
IsratJn <israt9626@gmail.com>
2021-09-14 02:33:15 +0600

    update

--- Object/object.js
+++ Object/object.js
@@ -69,3 +69,22 @@ userX.f();
 admin.f(); 
 
 admin['f']();
+
+//Create a Calculator
+let calculator = {
+    sum(){
+        return this.a + this.b;
+    },
+    mul(){
+        return this.a*this.b;
+    },
+    read() {
+        this.a =+prompt('a?',0);
+        this.b =+prompt('b?',0);
+    }
+};
+
+calculator.read();
+console.log(calculator.sum());
+console.log(calculator.sum());
+
