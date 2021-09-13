729de1212b553c3e12f2280f434cdf5ac58775b6
IsratJn <israt9626@gmail.com>
2021-09-14 00:29:49 +0600

    Rearranged

--- Object/object.js
+++ Object/object.js
@@ -19,10 +19,10 @@ let key = prompt("What do you want to know about the user?", "name", "age");
 console.log(user1[key]);
 
 
-let user = {};
-user.name = ["israt"];
-user["surname"] = ["Jahan"];
-user.name = ["Pete"];
-delete user.name;
+let user2 = {};
+user2.name = ["israt"];
+user2["surname"] = ["Jahan"];
+user2.name = ["Pete"];
+delete user2.name;
 
-console.log(user);
+console.log(user2);
