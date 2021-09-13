ac9a37159ba68f35c710ad7b1d83547c9f97ffbc
Pathao Ltd <pathaoltd@Pathaos-MacBook-Air.local>
2021-09-13 12:09:17 +0600

    array

---
+++ JS Fundamentals Part 2/Array/array.js
@@ -0,0 +1,9 @@
+const favoriteMovies = ['Asha Jawar Majhe', 'Wake up sid', 'Titli'];
+console.log("The first element:" + favoriteMovies[0]);
+console.log("The last element:" + favoriteMovies[favoriteMovies.length - 1]);
+
+
+const Israt = ['Israt', 2021 - 1998, favoriteMovies];
+console.log("Name:" + Israt[0]);
+console.log("Age:" + Israt[1]);
+console.log("Favorite movies are:" + Israt[Israt.length - 1]);
