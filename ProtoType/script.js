1cb089d1e7efd91e0ca7f991ca516439b01f021a
IsratJn <israt9626@gmail.com>
2021-09-15 04:43:01 +0600

    basic concepts

--- ProtoType/script.js
+++ ProtoType/script.js
@@ -32,3 +32,35 @@ let animal = {
   };
   
   rabbit.walk();
+
+  //getter and setter
+
+  let user ={
+      name:"Konka",
+      surName:"Vaiya",
+      get fullName(){
+          return `${this.name} ${this.surName}`
+      }
+  }
+
+  console.log(user.fullName);
+
+  //for..each
+  let animals = {
+    eats: true
+  };
+  
+  let monkey = {
+    jumps: true,
+    __proto__: animal
+  };
+  
+  for(let prop in monkey) {
+    let isOwn = monkey.hasOwnProperty(prop);
+  
+    if (isOwn) {
+      alert(`Our: ${prop}`); // Our: jumps
+    } else {
+      alert(`Inherited: ${prop}`); // Inherited: eats
+    }
+  }
