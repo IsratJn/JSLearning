a7c94125da6d0401d4145f48a35567887f8485b6
Israt33 <42838747+IsratJn@users.noreply.github.com>
2021-09-21 19:03:25 +0600

    DOM manipulation

---
+++ DOM/Guess my name/practice.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = "Correct Number!";
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.between').textContent= "Between 1 to 100";
+document.querySelector('.score').textContent= '10';
+document.querySelector('.guess').value='45';
+console.log(document.querySelector('.guess').value);
